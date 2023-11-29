ee7befb1a5d76e4615524c302e5cb51fbea9475a
Pierluigi <pierluigi.capirci89@gmail.com>
2023-11-29 11:40:10 +0000

    created frontpage

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl : '../src/assets/images/hats.jpg'
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl : '../src/assets/images/jackets.jpg'
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl : '../src/assets/images/sneakers.jpg'
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl : '../src/assets/images/women.jpg'
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl : '../src/assets/images/men.jpg'
+    },
+  ];
+
+  export default categories;
