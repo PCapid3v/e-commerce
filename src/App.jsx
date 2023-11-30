88807a245713ed3f9d75f847744f7d2bf359dea8
Pierluigi <pierluigi.capirci89@gmail.com>
2023-11-30 12:23:29 +0000

    created google sign in and pushed in db

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Home from './components/routes/home/Home'
 import { Route, Routes } from 'react-router-dom'
+import SignIn from './components/routes/sign-in/Sign-in'
 import Navigation from './components/routes/navigation/Navigation'
+
 
 function App() {
   
     return (
+      <>
+      <Navigation />
       <Routes>
-      <Route path='/' index element={<Navigation />} />
-      <Route path='/home' element={<Home />} />
+      <Route path='/' index element={<Home />} />
+      <Route path='/sign-in' element={<SignIn/>} />
     </Routes>
+      </>
   )
 }
 
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD8YQV4fdYn6VGaJjpw0qgL-4B04c-RO2k",
+    authDomain: "pc-shop-db.firebaseapp.com",
+    projectId: "pc-shop-db",
+    storageBucket: "pc-shop-db.appspot.com",
+    messagingSenderId: "420152547891",
+    appId: "1:420152547891:web:4fd43b66362ed7b73a6146"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
